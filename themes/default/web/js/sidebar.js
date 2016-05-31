3eae0dab5e13cb692b7f2dd0f0be8b312e0f8df4
Sergey Uchaev <dialoggg1994@gmail.com>
2016-06-01 00:17:31 +0700

    Скрипт для моб

--- themes/default/web/js/sidebar.js
+++ themes/default/web/js/sidebar.js
@@ -1,11 +1,18 @@
-$(window).scroll(function()
-{
+$(document).ready(function () {
+    if ($(window).width() < '1280') {
 
-        if (window.scrollY > 160)
-	{
-		$(".navigation").addClass("navigation-fixed").css('top',-160);
-	} else
-{
- $(".navigation").removeClass("navigation-fixed").css('top',0);
-}
-;})
+        $(".description-building").css('top', -150);
+    }
+    else {
+    }
+})
+
+$(window).scroll(function () {
+
+    if (window.scrollY > 160) {
+        $(".navigation").addClass("navigation-fixed").css('top', -160);
+    } else {
+        $(".navigation").removeClass("navigation-fixed").css('top', 0);
+    }
+    ;
+})
