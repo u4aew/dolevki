998435af427f7577bc919bde158b1d05652d8edd
Pavel Prohorov <redperchic@mail.ru>
2016-06-30 11:11:24 +0700

    Ссылка на сайт

--- protected/modules/realty/views/apartment/list.php
+++ protected/modules/realty/views/apartment/list.php
@@ -17,7 +17,7 @@ function getUrl($sortAttribute)
     $requestString = http_build_query($getParams);
     $path = Yii::app()->request->pathInfo;
 
-    return "http://www.newdolevki.ru/".$path."?".$requestString."&Apartment_sort=".$sortAttribute;
+    return "http://www.dolevki-22.ru/".$path."?".$requestString."&Apartment_sort=".$sortAttribute;
 }
 
 ?>
