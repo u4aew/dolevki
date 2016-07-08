9cbb1df9cbaf5540d656f955104076b5ebaef69d
Pavel Prohorov <redperchic@mail.ru>
2016-07-08 15:39:58 +0700

    Кеширование страниц с выводом

--- protected/modules/realty/views/building/list.php
+++ protected/modules/realty/views/building/list.php
@@ -6,29 +6,36 @@
     <?php $this->renderPartial("/map/view",["url" => "/realty/realty/getBuildingsForMap", "map" => $map]); ?>
 <?php endif; ?>
 
-<div class="row">
-    <div class="col-lg-12">
-        <section>
-            <div class="grid">
-                <?php $this->widget(
-                    'bootstrap.widgets.TbListView',
-                    [
-                        'dataProvider' => $dataProvider,
-                        'itemView' => '/building/_item',
-                        'summaryText' => '',
-                        'enableHistory' => true,
-                        'cssFile' => false,
-                        'itemsCssClass' => 'row items',
-                     /*   'sortableAttributes' => [
-                            'sku',
-                            'name',
-                            'price',
-                            'update_time'
-                        ],*/
-                    ]
-                ); ?>
-            </div>
-        </section>
-    </div>
-</div>
 
+<?php if($this->beginCache(Yii::app()->controller->id.Yii::app()->controller->action->id.$dataProvider->pagination->currentPage)): ?>
+
+
+    <div class="row">
+        <div class="col-lg-12">
+            <section>
+                <div class="grid">
+                    <?php $this->widget(
+                        'bootstrap.widgets.TbListView',
+                        [
+                            'dataProvider' => $dataProvider,
+                            'itemView' => '/building/_item',
+                            'summaryText' => '',
+                            'enableHistory' => true,
+                            'cssFile' => false,
+                            'itemsCssClass' => 'row items',
+                            /*   'sortableAttributes' => [
+                                   'sku',
+                                   'name',
+                                   'price',
+                                   'update_time'
+                               ],*/
+                        ]
+                    ); ?>
+                </div>
+            </section>
+        </div>
+    </div>
+
+
+    <?php $this->endCache(); ?>
+<?php endif; ?>
